a5d0a9af8d9e4088ef643fc274a529a76c9b7112
Victoria Mahoney <63237@psdschools.org>
2022-01-24 14:09:16 -0700

    Initial Commit

---
+++ Hello World 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World 2.0
+//
+//  Created by Victoria Mahoney on 1/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hello World 2.0UITests/Hello_World_2_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hello_World_2_0UITestsLaunchTests.swift
+//  Hello World 2.0UITests
+//
+//  Created by Victoria Mahoney on 1/24/22.
+//
+
+import XCTest
+
+class Hello_World_2_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
