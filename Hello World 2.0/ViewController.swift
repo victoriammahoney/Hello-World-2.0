54436a17c33cd2d5167bef30b3eecea81092bf21
Victoria Mahoney <63237@psdschools.org>
2022-01-25 13:40:09 -0700

    completed Hello World Project

--- Hello World 2.0/ViewController.swift
+++ Hello World 2.0/ViewController.swift
@@ -7,11 +7,38 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var classLabel: UILabel!
+    
+    @IBOutlet weak var clickButton: UIButton!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let pearImageView = UIImage(named: "pear")
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var newLabel: UILabel!
+    
+    
+    @IBOutlet weak var bandLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let favoriteBand = "Imagine Dragons"
+        
+        newLabel.text = favoriteBand
+        
+        imageView.image = pearImageView
+        
+        let numOfMembers = "4"
+        
+        bandLabel.text = "Imagine Dragons has " + numOfMembers + " members."
+        
     }
 
 
